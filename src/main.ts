88187bdd73cc278d6651f923cd6b0acb0284a101
Marian <mocanumarian1993@gmail.com>
2023-11-08 19:24:34 +0100

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // credentials: true, // Optional: Allow cookies and authentication headers
+  });
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
