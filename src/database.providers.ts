fd8d42a4207b6596309f07b9bece6288a108d1db
Marian <mocanumarian1993@gmail.com>
2023-11-09 09:11:52 +0100

    formatting

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -3,7 +3,6 @@ import * as mongoose from 'mongoose';
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://127.0.0.1:27017/hotel'),
+    useFactory: (): Promise<typeof mongoose> => mongoose.connect('mongodb://127.0.0.1:27017/hotel'),
   },
 ];
