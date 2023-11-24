74c8cc64789735e9bc2882b39e5fd1138efd9825
Marian <mocanumarian1993@gmail.com>
2023-11-24 13:17:25 +0100

    added default port

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
     origin: 'http://localhost:3000', // Replace with your frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     // credentials: true, // Optional: Allow cookies and authentication headers
   });
-  const port = process.env.PORT
+  const port = process.env.PORT || 4200;
   await app.listen(port);
-  console.log(new Date())
+  console.log(new Date());
   console.log(`Server is running and listening on port ${port}`);
 }
 bootstrap();
