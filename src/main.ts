432df01a03fb32335cda2aac52da791af42da9a7
Marian <mocanumarian1993@gmail.com>
2024-01-15 15:15:38 +0100

    allowed all origins

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost', // Replace with your frontend origin
+    origin: true, // Replace with your frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Optional: Allow cookies and authentication headers
   });
