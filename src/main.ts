ab683b335f0c495cbe8be55c29a4ca341a6f2e8e
Marian <mocanumarian1993@gmail.com>
2024-01-15 14:38:45 +0100

    updated database conection for scripts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  }});
 
   app.enableCors({
     origin: 'http://localhost:3000', // Replace with your frontend origin
